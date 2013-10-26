f87efd3893151e06262e884fbdb069dcc0eddaf2
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-10-26 07:08:03 +0100

    Built the most degnerate test case: the gutter game and the code to make it pass

--- BowlingGame/src/com/butunclebob/BowlingGame.java
+++ BowlingGame/src/com/butunclebob/BowlingGame.java
@@ -2,4 +2,13 @@ package com.butunclebob;
 
 public class BowlingGame {
 
+	public void roll(int pins) {
+		
+	}
+
+
+	public int score() {
+		return 0;
+	}
+
 }
--- BowlingGame/src/com/butunclebob/BowlingGameTest.java
+++ BowlingGame/src/com/butunclebob/BowlingGameTest.java
@@ -1,13 +1,14 @@
 package com.butunclebob;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.core.IsEqual.equalTo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
+	private BowlingGame g;
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -17,8 +18,18 @@ public class BowlingGameTest {
 	}
 
 	@Test
-	public void nothing() {
+	public void gutterGame() {
+		g = new BowlingGame();
 		
+		rollMany(20, 0);
+		
+		assertThat(g.score(), equalTo(0));
+	}
+	
+	private void rollMany(int n, int pins){
+		for(int i = 0; i < 20; i++){
+			g.roll(0);
+		}
 	}
 
 }
